9376321768289d5a0c5afd9b3ee4c08c5a97a9c9
Tommy Groshong <tom@groshong.software>
2023-08-22 15:27:31 -0400

    Add code generation for visitor interface

--- app/src/main/java/jtomahawk/ast/Expr.java
+++ app/src/main/java/jtomahawk/ast/Expr.java
@@ -4,8 +4,18 @@ import java.util.List;
 import jtomahawk.Token;
 
 abstract class Expr {
-  static class Binary extends Expr { 
-    Binary(Expr left, Token operator, Expr right){ 
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+
+    R visitGroupingExpr(Grouping expr);
+
+    R visitLiteralExpr(Literal expr);
+
+    R visitUnaryExpr(Unary expr);
+  }
+
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
@@ -16,24 +26,24 @@ abstract class Expr {
     final Expr right;
   }
 
-  static class Grouping extends Expr { 
-    Grouping(Expr expression){ 
+  static class Grouping extends Expr {
+    Grouping(Expr expression) {
       this.expression = expression;
     }
 
     final Expr expression;
   }
 
-  static class Literal extends Expr { 
-    Literal(Object value){ 
+  static class Literal extends Expr {
+    Literal(Object value) {
       this.value = value;
     }
 
     final Object value;
   }
 
-  static class Unary extends Expr { 
-    Unary(Token operator, Expr right){ 
+  static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
     }
@@ -43,4 +53,3 @@ abstract class Expr {
   }
 
 }
-
