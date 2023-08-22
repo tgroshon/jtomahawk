aa7f870cc4fe33e3602d5dabbe4fed1a5efe37ba
Tommy Groshong <tom@groshong.software>
2023-08-22 15:33:43 -0400

    Refactor some ast generation

--- app/src/main/java/jtomahawk/ast/Expr.java
+++ app/src/main/java/jtomahawk/ast/Expr.java
@@ -6,16 +6,13 @@ import jtomahawk.Token;
 abstract class Expr {
   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
   }
 
-  static class Binary extends Expr {
-    Binary(Expr left, Token operator, Expr right) {
+  static class Binary extends Expr { 
+    Binary(Expr left, Token operator, Expr right) { 
       this.left = left;
       this.operator = operator;
       this.right = right;
@@ -26,24 +23,24 @@ abstract class Expr {
     final Expr right;
   }
 
-  static class Grouping extends Expr {
-    Grouping(Expr expression) {
+  static class Grouping extends Expr { 
+    Grouping(Expr expression) { 
       this.expression = expression;
     }
 
     final Expr expression;
   }
 
-  static class Literal extends Expr {
-    Literal(Object value) {
+  static class Literal extends Expr { 
+    Literal(Object value) { 
       this.value = value;
     }
 
     final Object value;
   }
 
-  static class Unary extends Expr {
-    Unary(Token operator, Expr right) {
+  static class Unary extends Expr { 
+    Unary(Token operator, Expr right) { 
       this.operator = operator;
       this.right = right;
     }
@@ -53,3 +50,4 @@ abstract class Expr {
   }
 
 }
+
